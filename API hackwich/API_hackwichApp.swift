f1967ca91da17e83c90d1eb4948d7732da35491d
chris robles <croblesquezad@bsd220.org>
2024-03-04 09:32:51 -0600

    Initial Commit

---
+++ API hackwich/API_hackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_hackwichApp.swift
+//  API hackwich
+//
+//  Created by christopher robles quezada on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct API_hackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
